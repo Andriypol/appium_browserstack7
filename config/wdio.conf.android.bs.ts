a70fa79e8f8e9820963370857a29e3044faeba18
turtulian <turtulian@gmail.com>
2025-02-27 22:24:38 +0200

    changed yaml appium 276.02 17

--- config/wdio.conf.android.bs.ts
+++ config/wdio.conf.android.bs.ts
@@ -12,10 +12,6 @@ export const config = {
           deviceName: 'Oppo Reno 3 Pro',
           platformVersion: '10.0',
           platformName: 'android',
-          skipServerInstallation: true,
-          ignoreHiddenApiPolicyError: true,
-          noReset: true,
-          disableIdLocatorAutocompletion: true
         }
       }],
     services: [
