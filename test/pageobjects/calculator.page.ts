94b498a98603108c5e2fb81c38aafe49c926e89d
turtulian <turtulian@gmail.com>
2025-02-28 12:11:49 +0200

    changed yaml appium 276.02 1 22

--- test/pageobjects/calculator.page.ts
+++ test/pageobjects/calculator.page.ts
@@ -36,7 +36,9 @@ class CalculatorPage extends Page {
   async enterNumber(number: string): Promise<void> {
     for (const digit of number) {
         // Using the id= prefix to specify the selector strategy
-        await this.getElement(`id=com.google.android.calculator:id/digit_${digit}`).click();
+        const element = await this.getElement(`id=com.google.android.calculator:id/digit_${digit}`);
+        await element.waitForDisplayed({ timeout: 3000 });
+        await element.click();
     }
 }
 
